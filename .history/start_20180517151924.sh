8c1ecbd6482f394dc1c0b7b8ff9af13f1f47e766
dlee20013 <dlee20013@gmail.com>
2018-05-17 15:24:38 -0700

    script for starting different containers for testing

---
+++ .history/start_20180517151924.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -n "$1" ] && [ -n "$2" ]
+then
+  
+  INITCONTAINER=$(docker ps -q)
+  if [ -n "$INITCONTAINER" ]
+  then
+    docker kill $(docker ps -aq)
+  fi
+  echo "Running docker test for the directory: \"$1\" and port number \"$2\""
+  docker build "$1"
+  IMAGE=$(docker image ls -q | head -1)
+  docker run -it -d -p $2:$2 $IMAGE 
+  CONTAINER=$(docker ps -q | head -1)
+  docker exec -it $CONTAINER bash
+else  
+  echo "Please provide parameter: (./start.sh [directory name] [port number])"
+fi
